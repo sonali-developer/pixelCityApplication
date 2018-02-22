b3e63025884f41ab5598dd599a52c4e9a227510c
SONALI PATEL <patelsonali123@yahoo.com>
2018-02-21 21:31:18 -0500

    Successfully did project setup for pixel city

---
+++ PixelCityApp/PixelCityApp/Controller/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  PixelCityApp
+//
+//  Created by Sonali Patel on 2/21/18.
+//  Copyright © 2018 Sonali Patel. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
