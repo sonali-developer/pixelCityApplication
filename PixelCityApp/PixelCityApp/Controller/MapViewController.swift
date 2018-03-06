443a62e4f347e3484fec98cd0288508dd5567435
SONALI PATEL <patelsonali123@yahoo.com>
2018-03-06 02:34:23 -0500

    Finished working with requesting location services

--- PixelCityApp/PixelCityApp/Controller/MapViewController.swift
+++ PixelCityApp/PixelCityApp/Controller/MapViewController.swift
@@ -8,21 +8,58 @@
 
 import UIKit
 import MapKit
-class MapViewController: UIViewController {
+import CoreLocation
 
+class MapViewController: UIViewController {
    
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    
+    let regionRadius: Double = 1000
     override func viewDidLoad() {
         super.viewDidLoad()
        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
     
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            print(authorizationStatus.rawValue)
+            print(authorizationStatus.hashValue)
+            centerMapOnUserLocation()
+        }
     }
 
 }
 
 extension MapViewController : MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapViewController : CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+            print("Requested\(authorizationStatus.rawValue)")
+        } else {
+            print("Available\(authorizationStatus.rawValue)")
+            return
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+        print(authorizationStatus.rawValue)
+    }
+    
+    
+    
     
 }
 
